20bfb05551b3a200176c93d8a752c383ba75622d
Asrar <asrarazim15@gmail.com>
2026-04-13 14:02:18 +0600

    Disabled restricted mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    formats: ["image/webp"],
+  },
 };
 
 export default nextConfig;
